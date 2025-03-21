b4665b6db93b7383373f8695d4d9167a99115fb8
khanhne1304 <chikhanhphanle2004@gmail.com>
2025-03-21 09:25:24 +0700

    add ProductAdapter.java Product.java

---
+++ App/app/src/main/java/com/example/retrofit/model/Product.java
@@ -0,0 +1,52 @@
+package com.example.retrofit.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Product {
+    private int id;
+    @SerializedName("name")
+    private String name;
+    @SerializedName("image")
+    private String image;
+    @SerializedName("price")
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product(int id, String name, String image, double price) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+    }
+}
